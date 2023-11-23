0642bcd0ba52da84f63dd7f25c42c2dbc5e8bca4
SubodhSingh8543 <subodhsingh8543@gmail.com>
2023-11-23 14:29:21 +0530

    search pacakages done

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,8 +1,48 @@
+import { Box, Button, Flex, Input, Radio, RadioGroup, Stack, Text, Textarea } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-const Home = () =>{
+const Home = () => {
+    const [pacakage, setPackage] = useState("");
+    const [searchRes, setSearchRes] = useState(["reactjs", "react-redux", "react-i18next", "angular", "react-router-dom", "chakra-ui", "axios"]);
+    const [value, setValue] = useState(null);
+    const [textarea, setTextArea] = useState("");
+    const [filtValue,setFilt] = useState(searchRes)
 
-    return(
-       <div>hello</div> 
+    useEffect(() => {
+        let val = searchRes.filter((el,i) => {
+            let newEl = "#" + el + "#";
+            newEl = newEl.split(pacakage);
+            if(newEl.length > 1){
+                return el;
+            }
+        })
+        setFilt(val || []);
+
+    },[pacakage])
+
+    console.log(filtValue);
+    return (
+        <>
+            <Box pl={"20%"} pr={"20%"} mt={"150px"}>
+                <Text textAlign={"start"}>Search for NPM Packages</Text>
+                <Input placeholder="angular" value={pacakage} onChange={(e) => setPackage(e.target.value)} />
+                <Box mt={"20px"}>
+                    <RadioGroup onChange={setValue} value={value}>
+                        <Stack direction='column' maxH={"150px"} minH={"150px"} overflow={"scroll"}>
+                            {filtValue.map((el, i) => (
+                                <Radio key={i} value={el}>{el}</Radio>
+                            ))}
+                        </Stack>
+                    </RadioGroup>
+                </Box>
+                <Text textAlign={"start"} mt={"20px"} >why is this is your fav</Text>
+                <Textarea value={textarea} onChange={(e) => setTextArea(e.target.value)}  />
+
+                <Flex justifyContent={"flex-end"}>
+                    <Button bgColor={"#6558f5"} color={"white"} _hover={{bgColor:"#877fff"}} mt={"20px"} alignSelf={"end"}>Submit</Button>
+                </Flex>
+            </Box>
+        </>
     )
 }
 
